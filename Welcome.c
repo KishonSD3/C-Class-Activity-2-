7a5f1734cd41bd8d13eea801fd9449178fe88b89
Kishon Daniels <kishondaniels@gmail.com>
2015-08-24 10:59:33 -0500

    Create Welcome.c
    First class activity to show short message on the screen.

---
+++ Welcome.c
@@ -0,0 +1,18 @@
+Enter file contents here
+
+/*programmer: Kishon Daniels
+              Date:August 22, 2015
+              Purpose: print out welcome message on the screen
+*/
+
+//Program starts
+
+#include <stdio.h>
+int main (void)
+{
+
+  printf("Welcome to C!\n");
+  
+  return 0;
+
+}
